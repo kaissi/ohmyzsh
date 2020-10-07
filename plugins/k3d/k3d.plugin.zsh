7e0b494df366f52f57204b909d7ac90da737d4a9
kaissi <mrkaissi@gmail.com>
2020-10-07 17:27:27 -0300

    k3d plugin script corrected

--- plugins/k3d/k3d.plugin.zsh
+++ plugins/k3d/k3d.plugin.zsh
@@ -2,7 +2,7 @@ if (( $+commands[k3d] )); then
     __K3D_COMPLETION_FILE="${ZSH_CACHE_DIR}/k3d_completion"
 
     if [[ ! -f $__K3D_COMPLETION_FILE ]]; then
-        kubectl completion zsh >! $__K3D_COMPLETION_FILE
+        k3d completion zsh >! $__K3D_COMPLETION_FILE
     fi
 
     [[ -f $__K3D_COMPLETION_FILE ]] && source $__K3D_COMPLETION_FILE
